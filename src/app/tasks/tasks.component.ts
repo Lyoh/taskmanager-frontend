2dbfccbdce7ea7e79cb76ec8aee4a28d503719dc
Riubbe S. Oliveira <rb.oliveira@hotmail.com>
2017-09-20 17:57:59 -0300

    Adding Tasks Component

---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+
+const TASKS: Array<any> = [
+  { id: 1, title: 'Tarefa 1' },
+  { id: 2, title: 'Tarefa 2' },
+  { id: 3, title: 'Tarefa 3' },
+  { id: 4, title: 'Tarefa 4' },
+  { id: 5, title: 'Tarefa 5' }
+];
+
+@Component({
+  selector: 'tasks',
+  templateUrl: './tasks.component.html'
+})
+export class TasksComponent implements OnInit {
+
+  tasks: any;
+  
+  constructor() {
+    this.tasks = TASKS;
+   }
+
+  ngOnInit() { }
+
+}
