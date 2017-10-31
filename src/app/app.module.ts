1bc234237021712294361e7e8e856869e63f1446
Riubbe S. Oliveira <rb.oliveira@hotmail.com>
2017-10-31 16:35:25 -0200

    Incluling method for GetTask

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent  } from './navbar/navbar.component';
+
+import { AppRoutingModule } from './app.routing.module';
+import { TasksModule } from './tasks/tasks.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent
+  ],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    TasksModule
+  ],
+  bootstrap: [
+    AppComponent
+  ]
+})
+export class AppModule { }
--- src/app/tasks/tasks-detail/tasks-detail.component.ts
+++ src/app/tasks/tasks-detail/tasks-detail.component.ts
@@ -1,16 +1,33 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription, Operator } from 'rxjs/Rx';
 
 import { Tasks } from '../shared/tasks.model';
+import { TasksService } from '../shared/tasks.service';
 
 @Component({
   selector: 'app-task-detail',
   templateUrl: './tasks-detail.component.html'
 })
-export class TasksDetailComponent implements OnInit {
+export class TasksDetailComponent implements OnInit, OnDestroy {
 
   @Input()task: Tasks;
 
-  constructor() { }
+  subs: Subscription;
+  id: number;
 
-  ngOnInit(): void { }
+  constructor(
+    private tasksService: TasksService,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.subs = this.route.params
+                  .switchMap((params: Params) => this.tasksService.getTask(+params['id']) )
+                  .subscribe(task => this.task = task);
+  }
+
+  ngOnDestroy(): void {
+    this.subs.unsubscribe();
+  }
 }
