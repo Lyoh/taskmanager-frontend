90396419bf8085a0dfcb9e9d5d2c371e80e0bc59
Riubbe S. Oliveira <rb.oliveira@hotmail.com>
2017-11-22 14:19:14 -0200

    refactoring task detail

--- src/app/tasks/tasks-detail/tasks-detail.component.ts
+++ src/app/tasks/tasks-detail/tasks-detail.component.ts
@@ -9,16 +9,14 @@ import { TasksService } from '../shared/tasks.service';
 
 @Component({
   selector: 'app-task-detail',
-  templateUrl: './tasks-detail.component.html'
+  templateUrl: './tasks-detail.component.html',
+  styles: [".form-control-feedback{margin-right:20px}"]
 })
 export class TasksDetailComponent implements OnInit, OnDestroy, AfterViewInit {
   public reactiveTaskForm: FormGroup;
 
   task: Tasks;
-  doneOptions: Array<any> = [
-    { value: false, text: 'Pendente' },
-    { value: true, text: 'Concluída' }
-  ];
+  doneOptions: Array<any>;
 
   subs: Subscription;
   id: number;
@@ -29,6 +27,11 @@ export class TasksDetailComponent implements OnInit, OnDestroy, AfterViewInit {
     private location: Location,
     private formBuilder: FormBuilder
   ) {
+    this.doneOptions = [
+      { value: false, text: 'Pendente' },
+      { value: true, text: 'Concluída' }
+    ];
+
     this.reactiveTaskForm = this.formBuilder.group({
       title: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(255)]],
       deadline: [null, Validators.required],
@@ -52,22 +55,23 @@ export class TasksDetailComponent implements OnInit, OnDestroy, AfterViewInit {
     this.subs.unsubscribe();
   }
 
-  ngAfterViewInit() {
-    $('#deadline').datetimepicker({
+  public ngAfterViewInit(){
+    $("#deadline").datetimepicker({
       'sideBySide': true,
       'locale': 'pt-br'
-    }).on('db.change', () => this.reactiveTaskForm.get('deadline').setValue($('#deadline').val()));
-  // }).on('db.change', () => this.reactiveTaskForm.patchValue( { deadline: $('#deadline').val() } ));
+    }).on('dp.change', ()=> this.reactiveTaskForm.get('deadline').setValue( $("#deadline").val() ));
   }
 
-  /**
-   * goBack
-   */
   public goBack() {
     this.location.back();
   }
 
   public updateTask() {
+    this.task.title = this.reactiveTaskForm.get('title').value;
+    this.task.deadline = this.reactiveTaskForm.get('deadline').value;
+    this.task.done = this.reactiveTaskForm.get('done').value;
+    this.task.description = this.reactiveTaskForm.get('description').value;
+
     this.tasksService.update(this.task)
       .subscribe(
         () => alert('Tarefa atualizada com sucesso!!'),
@@ -75,12 +79,33 @@ export class TasksDetailComponent implements OnInit, OnDestroy, AfterViewInit {
       );
   }
 
-  public showFieldErrors(field): boolean {
-    return field.invalid && (field.touched || field.dirty)
+  public showFieldErrors(fieldName: string): boolean {
+    let field = this.getField(fieldName);
+    return field.invalid && (field.touched || field.dirty);
+  }
+
+  public getFieldClass(fieldName: string) {
+    return {
+      'has-error': this.showFieldErrors(fieldName),
+      'has-success': this.getField(fieldName).valid
+    };
+  }
+
+  public getIconClass(fieldName: string) {
+    return {
+      'glyphicon': true,
+      'form-control-feedback': true,
+      'glyphicon-remove': this.showFieldErrors(fieldName),
+      'glyphicon-ok': this.getField(fieldName).valid
+    };
+  }
+
+  public getField(fieldName: string) {
+    return this.reactiveTaskForm.get(fieldName);
   }
 
   private setTask(task: Tasks) {
+    this.task = task;
     this.reactiveTaskForm.patchValue(task);
   }
-
 }
